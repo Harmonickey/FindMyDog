61c8971cdfd6f60d0d95f89aaf6412d050972ea4
Alex Ayerdi <AlexanderAyerdi2015@u.northwestern.edu>
2014-05-08 12:13:35 -0500

    Register

--- js/index.js
+++ js/index.js
@@ -11,8 +11,8 @@ $(function () {
 	
 	//first grab the user cookie if there is one
 	var username = getCookie("username");
-	console.log(username);
-	getUser(username);
+	var password = getCookie("password");
+	getUserFromFirebase(username, password, 'mainscreen');
 });
 
 function getCookie(cname)
@@ -47,14 +47,17 @@ function hideLoginModal()
 	$('#loginModal').modal('hide');	
 }
 
-function checkFirebaseForLogin(username)
+function checkFirebaseForLogin(username, password, module)
 {
 	//check Firebase for login name	
-	if(getUser(username))
+	if(getUserFromFirebase(username, password, module))
 	{
 		//if good then change the Login to Logout and set variables, close loginModal
-		fillInFrontPage(phoneNumber, radius, baseLocation);
-		hideLoginModal();
+		if (module == 'login')
+		{
+			fillInFrontPage(phoneNumber, radius, baseLocation);
+			hideLoginModal();
+		}
 		return true;
 	}
 	else
@@ -66,8 +69,13 @@ function checkFirebaseForLogin(username)
 
 function createFirebaseUser(username, password)
 {
-	 myDataRef.child('user').set(username);
-	 myDataRef.child('user').child(username).child("Password").set(password);
+	 if (checkFirebaseForLogin(username, password, 'register'))
+	 {
+	 	myDataRef.child('user').child(username).set({'password': password});
+		return true;
+	 }
+	 
+	 return false;
 }
 
 function fillInFrontpage(phoneNumber, radius, baseLocation)
@@ -75,23 +83,72 @@ function fillInFrontpage(phoneNumber, radius, baseLocation)
 	
 }
 
-function getUser(username)
+function getUserFromFirebase(username, password, module)
 {
-	if (username)
+	if (username && password && module)
 	{
 		//check firebase for the user
-		var val = myDataRef.child('user').child(username);
-		console.log(val);
-		return true;
-		//setUser(username);
+		var firebaseAPI = "https://findmydeardog.firebaseio.com/user/" + username+ "/.json";
+		var ret_val = false;
+		$.ajax ({
+			dataType : "jsonp",
+			url : firebaseAPI,
+			async : false,
+			success : function(data) {
+				console.log("Data: " + data);
+		  		if (data != 'null' && data != null)
+				{
+					console.log("Module: " + module);
+					if (module == 'login')
+					{
+						if (data['Password'] == password)
+						{
+							setUser(username, data['Location'], data['Phone_Number'], data['Threshold']);
+							ret_val = true;
+						}
+						else
+						{
+							setError('no_pass', module);
+							ret_val = false;
+						}
+					}
+					else if (module == 'register')
+					{
+						ret_val = false;
+					}
+				}
+				else
+				{
+					console.log("Module: " + module);
+					if (module == 'login')
+					{
+						setError('no_user', module);
+						ret_val = false;
+					}
+					else if (module == 'register') 
+					{
+						ret_val = true;
+						console.log("Set Val: " + ret_val);
+					}
+				}
+			}
+		});
+		console.log(ret_val);
+		return ret_val;
 		//if there is a user then fill in the appropriate information on the frontpage elements and store variables
 		//fillInFrontpage(phoneNumber, radius, baseLocation);
 	} 
 	else 
 	{
+		setError('no_user', module);
 		return false;
 		//if no user, then fill in defaults	
 	}
+}
+
+function setUser(username, phoneNumber, radius)
+{
+	
 }
 
 function login()
@@ -99,10 +156,10 @@ function login()
 	var username = $("#username").val();
 	var password = $("#password").val();
 	
-	if (checkFirebaseForLogin(username))
+	if (checkFirebaseForLogin(username, password, 'login'))
 	{
-		setCookie("username", username, 30); 	
-		getUser(username);
+		setCookie("username", username, 30);
+		setCookie("password", password, 30);
 	}
 }
 
@@ -111,11 +168,42 @@ function register()
 	var username = $("#new_username").val();
 	var password = $("#new_password").val();
 	
-	createFirebaseUser(username, password);
+	if(createFirebaseUser(username, password))
+	{
+		hideRegisterModal();	
+	}
+	else
+	{
+		setError('no_user', 'register');	
+	}
+	
+	
 }
 
 function showRegisterModal()
 {
 	hideLoginModal();
 	$('#registerModal').modal('show');
+}
+
+function hideRegisterModal()
+{
+	$('#registerModal').modal('hide');
+}
+
+function setError(error, module)
+{
+	switch (module) {
+		case 'login':
+			$("#login_error").css('display', 'block');
+			if (error == 'no_user')
+				$("#login_error").html("Username not found");
+			else if (error == 'no_pass')
+				$("#login_error").html("Password not correct");		
+		case 'register':
+			$("#register_error").css('display', 'block');
+			if (error == 'no_user')
+				$("#register_error").html("Username already taken");
+		case 'mainscreen':
+	}
 }
