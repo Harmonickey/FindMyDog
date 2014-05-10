f5d07db5906f6ad54b934629bba55329a4bf41d8
Alex Ayerdi <AlexanderAyerdi2015@u.northwestern.edu>
2014-05-10 12:51:17 -0500

    Text Code

--- text.php
+++ text.php
@@ -1,9 +1,13 @@
 <?php
-
-shell_exec("curl -X POST 'https://api.twilio.com/2010-04-01/Accounts/AC461d3f5fa9b34098d83e19417d88608c/Messages.json' \
-	--data-urlencode 'To=269-267-3752'  \
-	--data-urlencode 'From=+12692042709'  \
-	--data-urlencode 'Body=Dog is running away!' \
-	-u AC461d3f5fa9b34098d83e19417d88608c:93e934c729223531780d8a0851d14ab3");
+// Get the PHP helper library from twilio.com/docs/php/install
+require_once('twilio-php/Services/Twilio.php'); // Loads the library
+ 
+// Your Account Sid and Auth Token from twilio.com/user/account
+$sid = "AC461d3f5fa9b34098d83e19417d88608c"; 
+$token = "93e934c729223531780d8a0851d14ab3"; 
+$client = new Services_Twilio($sid, $token);
+ 
+// From - To - Body
+$client->account->messages->sendMessage("+12692042709", "(269) 267-3752", "Your Dog is running away!");
 
 ?>
