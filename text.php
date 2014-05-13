f51ff88d912572ff60327ecdc07413cc8049bd30
Alex Ayerdi <AlexanderAyerdi2015@u.northwestern.edu>
2014-05-13 09:10:01 -0700

    Just MySQL Tests

--- text.php
+++ text.php
@@ -8,6 +8,26 @@ $token = "93e934c729223531780d8a0851d14ab3";
 $client = new Services_Twilio($sid, $token);
 
 // From - To - Body
-$client->account->messages->sendMessage($_POST['From'], $_POST['To'], $_POST['Body']);
+//$client->account->messages->sendMessage($_POST['From'], $_POST['To'], $_POST['Body']);
+
+$host="localhost";
+$port=3306;
+$socket="";
+$user="root";
+$password="";
+$dbname="locations";
+
+$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
+	or die ('Could not connect to the database server' . mysqli_connect_error());
+
+if ($result = $con->query("SELECT * FROM dog_locations")) {
+    printf("Select returned %d rows.\n", $result->num_rows);
+
+    /* free result set */
+    $result->close();
+}
+
+//$con->close();
+
 
 ?>
