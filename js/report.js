384e79b5e6c3797824011c2751f8132afc871ec2
Dane Stier <DaneStier@u.northwestern.edu>
2014-05-19 20:32:19 -0500

    asdf

--- js/report.js
+++ js/report.js
@@ -15,7 +15,7 @@ function getReportInfo(minTime, maxTime)
 	var query = new Parse.Query("Dog_Location");
 	var lat_long = new Array();
 	//if (getCookie('username')!=null) {
-		query.select("Location").equalTo("Username", 'dcs592').lessThanOrEqualTo("Time", maxTime).greaterThanOrEqualTo("Time", minTime).descending("Time").find({
+		query.select("Location").equalTo("Username", getCookie("username")).lessThanOrEqualTo("Time", maxTime).greaterThanOrEqualTo("Time", minTime).descending("Time").find({
 		  success: function(results) {
 			for (var i = 0; i < results.length; i++)
 			{
