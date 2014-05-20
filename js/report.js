1be2862cabb6ff171030fbc983f5aa60a4926e01
Dane Stier <DaneStier@u.northwestern.edu>
2014-05-19 20:34:17 -0500

    cookie

--- js/report.js
+++ js/report.js
@@ -10,6 +10,20 @@ $(function () {
 	
 });
 
+function getCookie(cname)
+{
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0; i<ca.length; i++) 
+	{
+		var c = ca[i].trim();
+		if (c.indexOf(name) == 0) 
+			return c.substring(name.length,c.length);
+	}
+	return null;
+}
+
+
 function getReportInfo(minTime, maxTime)
 {
 	var query = new Parse.Query("Dog_Location");
