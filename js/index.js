5192d936391247b42cb9f46b093f3e80d115c45b
Alex Ayerdi <AlexanderAyerdi2015@u.northwestern.edu>
2014-05-28 22:15:40 -0500

    Trying to get autocomplete to work

--- js/index.js
+++ js/index.js
@@ -40,6 +40,32 @@ $(function () {
 		}	
 	} 
 
+	//var input = $("#reg_baselocation");
+	var input2 = document.getElementById('new_baselocation');
+	
+	//var autocomplete = new google.maps.places.Autocomplete(input);
+	var autocomplete2 = new google.maps.places.Autocomplete(input2);
+	var infowindow = new google.maps.InfoWindow();
+	
+	google.maps.event.addListener(autocomplete2, 'place_changed', function() {
+		infowindow.close();
+		var place = autocomplete2.getPlace();
+		if (!place.geometry) {
+			return;
+		}
+		console.log(place);
+		var address = '';
+		if (place.address_components) {
+			address = [
+				(place.address_components[0] && place.address_components[0].short_name || ''),
+				(place.address_components[1] && place.address_components[1].short_name || ''),
+				(place.address_components[2] && place.address_components[2].short_name || '')
+			].join(' ');
+		}
+
+		infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+		infowindow.open();
+	});
 });
 
 function getCookie(cname)
