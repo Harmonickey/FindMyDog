c2a46b55e86428f5f4de59d788efaaeb9ea88dc1
Dane Stier <DaneStier@u.northwestern.edu>
2014-06-01 22:30:09 -0500

    I think it's fixed...

--- js/maps.js
+++ js/maps.js
@@ -133,9 +133,7 @@ setInterval(trackLocation, 3000); //regularly update the position of the dog on 
 
 function trackLocation() {
   if(convertBoolean(getCookie("initialized"))) {
-    console.log("initialized");
     if(getCookie("username")) {
-      console.log(getCookie("username"));
       if(convertBoolean(getCookie("follow_device"))) {
         getUserLocation();
         console.log(owner_location);
@@ -156,6 +154,9 @@ function trackLocation() {
       window.location = "index.html";
     }
   }
+  else if(!getCookie("username")) {
+    window.location = "index.html";
+  } 
 }
 
 
