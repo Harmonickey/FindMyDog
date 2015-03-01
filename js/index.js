44cc537bb043e6f86a1f8b4eec017740d2c366d5
Alex Ayerdi <AlexanderAyerdi2015@u.northwestern.edu>
2015-02-28 23:26:03 -0600

    clear errors on retries

--- js/index.js
+++ js/index.js
@@ -2,6 +2,11 @@ var myDataRef = new Firebase('https://findmydeardog.firebaseio.com/');
 var error_modules = ['#register_error', '#login_error', '#phonenumber_error', '#radius_error', '#personal_radius_error', '#baselocation_error'];
 
 $(function () {
+	
+	$('.modal-body input').change(function() {
+		clearErrors();
+	});
+	
 	$( "#settings-toggle" ).click(function() {
 	  $( "#settings-hide" ).animate({ height: "toggle" }, 500);
 	});
